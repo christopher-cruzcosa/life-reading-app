d4ec6ea4f1eac3953b0a12e7ba27ddc4cee60285
Garret Bright <garret@drinknewtopia.com>
2020-09-24 13:48:03 -0700

    add three random card, move click to fortune html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,31 @@
 $(function () {
 
     
-    $(document).on("click", ".card", function () {
+    $(document).on("click", ".tarot-card", function () {
      
-        var queryUrl = "https://tarot.howlcode.com/api/v1/cards";
-  
+        var queryUrl = "https://tarot.howlcode.com/api/v1/spreads/three_cards";
+        // var deckArray = [];
         // send ajax request for tarot cards
         $.ajax({
           url: queryUrl,
           method: "GET",
         }).then(function (data) {
-          // console.log(data[0].image);
-          // Log the tarot card data in the console
+           console.log(data);
+           for (var i = 0; i < 11; i++){
+
+          //   var tarotName = $("#card-name")
+          //   var tarotImg = $("#card-image")
+          //   var tarotRev = $("#card-reversed")
+          //   var tarotUpright = $("#card-upright")
+          //   var tarotSum = $("#card-summary")
+
+           
+          // $("#card-name").text(data[i].name);
+          // $("#card-image").text(data[i].image);
+          // $("#card-reversed").text(data[i].reversed);
+          // $("#card-upright").text(data[i].upright);
+          // $("#card-summary").text(data[i].summary);
+           }
         });
       });
     $(document).on("click", ".card-image", function () {
@@ -34,7 +48,7 @@ $(function () {
           url: queryUrl,
           method: "GET",
         }).then(function (data) {
-          console.log(data);
+          // console.log(data);
           // Log the tarot card data in the console
         });
       });
